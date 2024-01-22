e17687cb37f07fc81078853a9aa31858c31ba5ea
Abdulrahman Masood <71218149+Abouod@users.noreply.github.com>
2024-01-22 16:04:59 +0800

    Add files via upload

---
+++ Challenges/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+//   updateMonthlyChallenges();
+//   populateCategoryChallenges("Fitness", "fitnessChallengesContainer");
+//   populateCategoryChallenges("Nutrition", "nutritionChallengesContainer");
+//   populateCategoryChallenges("Wellness", "wellnessChallengesContainer");
+//   populateCategoryChallenges(
+//     "Environmental",
+//     "environmentalChallengesContainer"
+//   );
+// });
